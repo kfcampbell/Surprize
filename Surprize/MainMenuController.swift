68576bab4806048a3855b88bc316c8ef5e03ed5e
kfcampbell <keeg4n.campbell@gmail.com>
2017-04-22 20:36:48 -0700

    Initial commit.

---
+++ Surprize/MainMenuController.swift
@@ -0,0 +1,26 @@
+//
+//  MainMenuController.swift
+//  Surprize
+//
+//  Created by Keegan Campbell on 4/22/17.
+//  Copyright © 2017 Keegan Campbell. All rights reserved.
+//
+
+import Cocoa
+
+class MainMenuController: NSObject {
+    @IBOutlet weak var statusMenu: NSMenu!
+    
+    @IBAction func quitClicked(_ sender: NSMenuItem) {
+        NSApplication.shared().terminate(self)
+    }
+    
+    let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+    
+    override func awakeFromNib() {
+        let icon = NSImage(named: "statusIcon")
+        icon?.isTemplate = true // best for dark mode
+        statusItem.image = icon
+        statusItem.menu = statusMenu
+    }
+}
