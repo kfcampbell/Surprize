d1140aa5f0d9218ccb8af3556293144030d2b75c
kfcampbell <keeg4n.campbell@gmail.com>
2017-04-28 21:11:46 -0700

    Random image sizes work now.

--- Surprize/MainMenuController.swift
+++ Surprize/MainMenuController.swift
@@ -44,20 +44,30 @@ class MainMenuController: NSObject {
         DispatchQueue.global().async {
             let data = try? Data(contentsOf: url!) //make sure your image in this url does exist, otherwise unwrap in a if let check / try-catch
             DispatchQueue.main.async {
-                    let image = NSImage(data: data!)
+                let image = NSImage(data: data!)
                 
-                    // now save image
-                    let desktopURL = FileManager.default.urls(for: .desktopDirectory, in: .userDomainMask).first!
-                    let destinationURL = desktopURL.appendingPathComponent("my-image.png")
-                if (image?.pngWrite(to: destinationURL, options: .atomic))! {
-                        print("File saved")
+                // now save image
+                let desktopURL = FileManager.default.urls(for: .desktopDirectory, in: .userDomainMask).first!
+                let uniqueFilename = self.getUniqueImageFilename()
+                let destinationURL = desktopURL.appendingPathComponent(uniqueFilename)
+                if (image?.pngWrite(to: destinationURL, options: .withoutOverwriting))! {
+                    print("File saved")
                     
-                    let filePath = desktopURL.appendingPathComponent("my-image.png").relativePath
-                    self.changeWallpaper(path: filePath)
+                let filePath = desktopURL.appendingPathComponent(uniqueFilename).relativePath
+                self.changeWallpaper(path: filePath)
 
                 }
             }
         }
+    }
+    
+    func getUniqueImageFilename() -> String {
+        let date = Date()
+        let formatter = DateFormatter()
+        
+        // todo(kfcampbell): reflect the categories used in the answer here
+        formatter.dateFormat = "yyyy-MM-dd-hh-mm-ss"
+        return (formatter.string(from: date) + ".png")
     }
 
     
